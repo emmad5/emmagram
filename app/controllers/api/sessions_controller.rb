cbe7c8b2e3c41bdfe65bfbd25cecc704b6effe2b
Emma Deas <emmadeas9@gmail.com>
2019-03-23 13:33:31 -0700

    finish user auth backend

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,25 @@
 class Api::SessionsController < ApplicationController
 
     def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: {base: ['Invalid credentials']}, status: 401
+        end
     end
 
     def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json: {base: ['you are not logged in']}, status: 401
+        end
+
     end
-    
+
+
+
 end
