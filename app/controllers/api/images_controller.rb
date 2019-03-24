34bb142456068205b33456c99b8d759700e0d74e
Emma Deas <emmadeas9@gmail.com>
2019-03-24 15:24:02 -0700

    update routes

---
+++ app/controllers/api/images_controller.rb
@@ -0,0 +1,49 @@
+class Api::ImagesController < ApplicationController
+  
+
+  def create
+    @image = Image.new(image_params)
+    @image.user_id = current_user.id
+    
+    if @image.save
+      render :show
+    else
+      render json: @images.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+
+    if @image
+      render :show
+    else
+      render json: @image.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @image = current_user.images.find(params[:id])
+
+    if @image.update(image_params)
+      render :show
+    else
+      render json: @image.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+
+    if @image.destroy
+      render :show
+    else
+      render json: @image.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def image_params
+    params.require(:image).permit(:body, :image)
+  end
+end
