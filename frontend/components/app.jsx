a29cc15ed799d0ae23b373264d1d12ce091e8bc6
Emma Deas <emmadeas9@gmail.com>
2019-03-23 17:15:55 -0700

    work on components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>EmmaGram</h1>
+    </div>
+)
+
+export default App;
---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+class Greeting extends React.Component {
+    render() {
+        return (
+            <nav>
+                Greeting
+            </nav>
+        )
+    }
+}
